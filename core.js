91a89c05b6d1e75813fc80bf12af24ea22865f62
Alx <a.sergienko@hadrout.com>
2016-01-18 16:46:58 +0300

    Add parallel function

--- core.js
+++ core.js
@@ -1,30 +1,39 @@
-function series(promises) {
-  "use strict";
+"use strict";
 
-  let isGenerator = false;
+function isGeneratorFunction(target) {
+  return target.constructor.name === 'GeneratorFunction';
+}
 
-  if (promises.constructor.name === 'GeneratorFunction') {
-    isGenerator = true;
-    promises = promises();
+function nextTask(target) {
+  if (isGeneratorFunction(target)) {
+    return target.next().value;
+  } else {
+    return target.shift();
   }
-  if (!isGenerator && typeof promises.shift !== "function") {
+}
+
+function series(promises) {
+  let isGenerator = isGeneratorFunction(promises);
+
+  if (isGenerator) {
+    promises = promises();
+  } else if (typeof promises.shift !== "function") {
     throw new TypeError("series must implement shift() or be Generator");
   }
 
   function step() {
     return new Promise((resolve, reject) => {
-      let currentPromise;
-      if (isGenerator) {
-        currentPromise = promises.next().value;
-      } else {
-        currentPromise = promises.shift();
-      }
+      let currentPromise = nextTask(promises);
 
       if (currentPromise === undefined) {
         resolve();
       }
 
-      Promise.resolve(isGenerator ? currentPromise : currentPromise())
+      if (!isGenerator) {
+        currentPromise = currentPromise();
+      }
+
+      Promise.resolve(currentPromise)
       .then(step)
       .then(resolve)
       .catch(reject);
@@ -34,12 +43,62 @@ function series(promises) {
   return step();
 }
 
+function parallel(promises) {
+  // Max parallel tasks
+  let parallelLimit = 100;
+
+  let isGenerator = isGeneratorFunction(promises);
+  let results = [];
+  let tasks = [];
+
+  if (isGenerator) {
+    promises = promises();
+  } else if (typeof promises.shift !== "function") {
+    throw new TypeError("series must implement shift() or be Generator");
+  }
+
+  let i = 0;
+
+  do {
+    let num = i++;
+    let currentPromise = nextTask(promises);
+
+    if (currentPromise === undefined) {
+      break;
+    }
+
+    if (num > parallelLimit) {
+      throw new Error("Limit of parallel promises exhausted");
+    }
+
+    if (!isGenerator) {
+      currentPromise = currentPromise();
+    }
+
+    let task = Promise.resolve(currentPromise)
+    .then((result) => {
+      results[num] = result;
+    });
+
+    results.push(undefined);
+    tasks.push(task);
+  } while (true);
+
+  return Promise.all(tasks)
+    .then(() => {
+      return results;
+    });
+}
+
 function extendPromise(promise) {
   if (typeof promise == "undefined") {
     throw new Error("Parameter Promise required");
   }
   if (typeof promise.series !== "function") {
     promise.series = series;
+  }
+  if (typeof promise.parallel !== "function") {
+    promise.parallel = parallel;
   }
 }
 
